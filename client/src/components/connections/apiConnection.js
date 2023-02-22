9358690912110ec29c3e4be62a6a00cb7be9f564
astrby <anthony.rezu@gmail.com>
2023-02-21 21:38:08 -0600

    Edición de links de axios a página hosteada en Vercel

--- client/src/components/connections/apiConnection.js
+++ client/src/components/connections/apiConnection.js
@@ -1,29 +1,29 @@
 import axios from 'axios'
 
 export const getPosts = async(setPosts) =>{
-  const peticion = await axios.get('http://localhost:3001/getPosts');
+  const peticion = await axios.get('https://react-wegram.vercel.app/getPosts');
   setPosts(peticion.data);
 }
 
 export const getIdPosts = async(userId, setPosts) =>{
-  const peticion = await axios.post('http://localhost:3001/getIdPosts', {userId: userId});
+  const peticion = await axios.post('https://react-wegram.vercel.app/getIdPosts', {userId: userId});
   setPosts(peticion.data);
 }
 
 export const getPost = async(id,setPost) =>{
-  const peticion = await axios.post('http://localhost:3001/getPost', {id: id})
+  const peticion = await axios.post('https://react-wegram.vercel.app/getPost', {id: id})
   setPost(peticion.data);
 };
 
 export const getLikes = async(loginData, setLikes) =>{
   if(loginData.length >0){
-    const peticion = await axios.post('http://localhost:3001/getLikes',{userId: loginData[0].id});
+    const peticion = await axios.post('https://react-wegram.vercel.app/getLikes',{userId: loginData[0].id});
     setLikes(peticion.data);
   }
 }
 
 export const getComments = async(postId, setComments)=>{
-  const peticion = await axios.post('http://localhost:3001/getComments',{
+  const peticion = await axios.post('https://react-wegram.vercel.app/getComments',{
     postId: postId
   })
   setComments(peticion.data)
