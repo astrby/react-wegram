e18d92aa289eee82dc7b75f3316dc832895707dd
astrby <anthony.rezu@gmail.com>
2023-02-26 17:15:22 -0600

    Cambio de host de backend

--- client/src/components/connections/apiConnection.js
+++ client/src/components/connections/apiConnection.js
@@ -1,36 +1,36 @@
 import axios from 'axios'
 
 export const getPosts = async(setPosts) =>{
-  const peticion = await axios.get('https://react-wegram.vercel.app/getPosts');
+  const peticion = await axios.get('https://react-wegram-backend.onrender.com/getPosts');
   setPosts(peticion.data);
 }
 
 export const getIdPosts = async(userId, setPosts) =>{
-  const peticion = await axios.post('https://react-wegram.vercel.app/getIdPosts', {userId: userId});
+  const peticion = await axios.post('https://react-wegram-backend.onrender.com/getIdPosts', {userId: userId});
   setPosts(peticion.data);
 }
 
 export const getPost = async(id,setPost) =>{
-  const peticion = await axios.post('https://react-wegram.vercel.app/getPost', {id: id})
+  const peticion = await axios.post('https://react-wegram-backend.onrender.com/getPost', {id: id})
   setPost(peticion.data);
 };
 
 export const getLikes = async(loginData, setLikes) =>{
   if(loginData.length >0){
-    const peticion = await axios.post('https://react-wegram.vercel.app/getLikes',{userId: loginData[0].id});
+    const peticion = await axios.post('https://react-wegram-backend.onrender.com/getLikes',{userId: loginData[0].id});
     setLikes(peticion.data);
   }
 }
 
 export const getComments = async(postId, setComments)=>{
-  const peticion = await axios.post('https://react-wegram.vercel.app/getComments',{
+  const peticion = await axios.post('https://react-wegram-backend.onrender.com/getComments',{
     postId: postId
   })
   setComments(peticion.data)
 }
 
 export const postLike = async(userId, postId)=>{
-  await axios.post('https://react-wegram.vercel.app/postLike',{
+  await axios.post('https://react-wegram-backend.onrender.com/postLike',{
     userId: userId, postId: postId
   })
 }
